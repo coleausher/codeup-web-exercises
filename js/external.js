3f603c46b3dcbc8abed757d318599dda40a34ce6
Cole Usher <coleausher97@gmail.com>
2023-02-17 12:32:54 -0600

    Js with html lecture notes

---
+++ js/external.js
@@ -0,0 +1,17 @@
+"use strict";
+
+console.log("this is my external js file");
+let quantity = 3;
+console.log(quantity);
+
+let myAge = 25;
+console.log(myAge);
+
+// alert("Have a good day")
+
+let submit = confirm("Are you sure you want to resubmit this form?");
+console.log(submit);
+
+let age = prompt("what is your age?")
+alert("Wow, you're old. You're " + age)
+
