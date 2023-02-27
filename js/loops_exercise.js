ad94b4b9f3790dd1ccbf72478895f0f2fda16d7b
Cole Usher <coleausher97@gmail.com>
2023-02-27 08:14:17 -0600

    loops exercise complete

--- js/loops_exercise.js
+++ js/loops_exercise.js
@@ -10,14 +10,15 @@ showMultiplicationTable(1)
 
 //Question 2 Use a for loop and the code from the previous lessons to generate 10 random numbers between 20 and 200 and output to the console whether each number is odd or even. For example:
 
-for (let a = 20; a <= 200; a++) {
-    if(a % 2 !== 0){
-        console.log(a + " is odd")
-    }else if(a % 3 !== 0){
-    console.log(a + " is even")
+for (let i = 0; i < 10; i++) {
+    let randomNum = Math.floor(Math.random() * (200 - 20 + 1) + 20);
+    if (randomNum % 2 === 0) {
+        console.log(randomNum + " is even");
+    } else if(randomNum % 3 === 0){
+        console.log(randomNum + " is odd");
     }
-
 }
+
 
 //Question 3 Create a for loop that uses console.log to create the output shown below
 //1
@@ -29,9 +30,11 @@ for (let a = 20; a <= 200; a++) {
 // 7777777
 // 88888888
 // 999999999
-for ( let c = 1; c < 10 ; c++) {
-    console.log(c + c)
+for (let c = 1; c < 10 ; c++) {
+   console.log(String(c).repeat(c))
 }
+
+
 //Question 4 Create a for loop that uses console.log to create the output shown below.
 //100
 // 95
@@ -54,6 +57,6 @@ for ( let c = 1; c < 10 ; c++) {
 // 10
 // 5
 
-// for (let b = 100; b >= 5; b-=5) {
-//     console.log(b)
-// }
+ for (let b = 100; b >= 5; b-=5) {
+     console.log(b)
+ }
