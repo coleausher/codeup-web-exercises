f43d2a6b508d5390df02aef905f2a3978c78bd27
Cole Usher <coleausher97@gmail.com>
2023-02-27 15:19:29 -0600

    planets array exercise complete

---
+++ js/planets_array.js
@@ -0,0 +1,54 @@
+"use strict";
+
+(function(){
+
+    let planets = [
+        'Mercury',
+        'Venus',
+        'Earth',
+        'Mars',
+        'Jupiter',
+        'Saturn',
+        'Uranus',
+        'Neptune'
+    ];
+
+    /**
+     * TODO:
+     * Read each console log below, and write some javascript code to perform
+     * the step that it describes
+     */
+// // .unshift adds to the beginning
+    let addSun = planets.unshift("The Sun");
+    console.log(addSun);
+    console.log(planets);
+
+// // .push adds to the end
+    let addPluto = planets.push("Pluto");
+    console.log(addPluto);
+    console.log(planets);
+
+// // .shift removes from the beginning
+   let removeSun = planets.shift();
+    console.log(removeSun);
+    console.log(planets);
+
+// // .pop removes from the end
+    let removePluto = planets.pop();
+    console.log(removePluto);
+    console.log(planets);
+
+//  // .indexOf finds the index of a item in the array
+   let index = planets.indexOf("Earth");
+    console.log(index);
+
+// // .reverse reverses the array
+   let reversePlanets = planets.reverse();
+    console.log(reversePlanets);
+    console.log(planets);
+
+// // .sort sorts alphanumerically
+    let alphanumericPlanets = planets.sort();
+    console.log(alphanumericPlanets);
+    console.log(planets);
+})();
