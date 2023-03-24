0a11240ff83e84d05af0548ac3836de1fc0af551
Cole Usher <coleausher97@gmail.com>
2023-03-24 13:48:36 -0500

    JQuery Into Lecture

---
+++ js/JQuery_intro_lecture.js
@@ -0,0 +1,11 @@
+// using JS Syntax
+// Waits for HTML page to load then fires off JS file
+// window.onload = function() {
+//     alert( 'The page has finished loading!' );
+// }
+
+// using JQuery Syntax
+// Waits for HTML page to load then fires off JS file
+$(function (){
+ alert("The page has finished loading!")
+});
